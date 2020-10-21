698f4826c70a21ca491cfadfc7dc22c319e9635a
Crevecoeur  Romain <r.crevecoeur@brgm.fr>
2020-10-21 14:51:56 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="q-app">
+    <q-calendar />
+  </div>
+</template>
+<script>
+import { QCalendar } from '@quasar/quasar-ui-qcalendar'
+
+export default {
+  components: {
+    QCalendar
+  }
+}
+</script>
